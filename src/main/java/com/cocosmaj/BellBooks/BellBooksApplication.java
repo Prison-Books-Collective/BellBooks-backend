75fa3e36075712b2003625442fb656e11e9c8d9f
Samah Majadla <smajadla@smajadla-a02.vmware.com>
2022-08-12 15:44:03 -0400

    first commit, empty project spring initialized

---
+++ src/main/java/com/cocosmaj/BellBooks/BellBooksApplication.java
@@ -0,0 +1,13 @@
+package com.cocosmaj.BellBooks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BellBooksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BellBooksApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cocosmaj/BellBooks/BellBooksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cocosmaj.BellBooks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BellBooksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

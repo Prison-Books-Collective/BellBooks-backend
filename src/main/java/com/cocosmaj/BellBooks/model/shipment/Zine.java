28f355ef922fc3da0b1eae6d2f7750affcb838cf
Coal Cooper <cocowmn@gmail.com>
2024-01-10 09:12:36 -0500

    update non-nullable columns with @NotNull annotation so non-null properties are visible in docs

--- src/main/java/com/cocosmaj/BellBooks/model/shipment/Zine.java
+++ src/main/java/com/cocosmaj/BellBooks/model/shipment/Zine.java
@@ -4,12 +4,13 @@ import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotNull;
 
 @Getter
 @Setter
 @Entity(name="zine")
 @DiscriminatorValue("zine")
 public class Zine extends PackageContent {
-
+    @NotNull
     private String code;
 }
